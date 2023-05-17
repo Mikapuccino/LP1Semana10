51a1cb3400c5c217eb6a333c8f786e3f97eccc12
Mikapuccino <nelsonfcmilheiro@gmail.com>
2023-05-17 18:40:14 +0100

    Create List, Stack, Queue and HashSet with the same five strings

--- StringCollections/Program.cs
+++ StringCollections/Program.cs
@@ -6,7 +6,35 @@ namespace StringCollections
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string string1 = "Ola";
+            string string2 = "mundo";
+            string string3 = "Ola";
+            string string4 = "Hello";
+            string string5 = "world";
+
+            list = new List<string>() {string1,
+            string2,
+            string3,
+            string4,
+            string5};
+
+            stack = new Stack<string>() {string1,
+            string2,
+            string3,
+            string4,
+            string5};
+
+            queue = new Queue<string>() {string1,
+            string2,
+            string3,
+            string4,
+            string5};
+
+            hashset = new HashSet<string>() {string1,
+            string2,
+            string3,
+            string4,
+            string5};
         }
     }
 }
