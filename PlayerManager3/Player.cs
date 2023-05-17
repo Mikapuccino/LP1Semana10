50337b15ebf41ed612922a330647464d7fc71003
Mikapuccino <nelsonfcmilheiro@gmail.com>
2023-05-17 16:33:46 +0100

    Player class implements IComparable<Player> interface

--- PlayerManager3/Player.cs
+++ PlayerManager3/Player.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace PlayerManager2
+namespace PlayerManager3
 {
-    public class Player
+    public class Player : IComparable<Player>
     {
         public string Name { get; }
         public int Score { get; set; }
@@ -14,6 +14,12 @@ namespace PlayerManager2
         {
             Name = name;
             Score = score;
+        }
+
+        public int CompareTo(Player other)
+        {
+            if (other == null) return 1;
+            return other.Score - this.Score;
         }
     }
 }
