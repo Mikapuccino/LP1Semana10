aef5e3a5b0d69b371df18e1bf6d7c2a29fd1a1f3
Nelson <nelsonfcmilheiro@gmail.com>
2023-05-19 13:05:16 +0100

    Add LootSort to solution

---
+++ LootSort/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LootSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
